c8da0d27cacf8fe0d6aa786d597630e2907a3d40
SamBarFu <samuelbarberena12@gmail.com>
2020-09-17 18:50:49 -0600

    home ready

---
+++ js/home.js
@@ -0,0 +1,10 @@
+var background = document.getElementById('fondo')
+
+window.onload = function() {
+
+    document.onmousemove = function(e) {
+        var x = -(e.clientX/80);
+        var y = -(e.clientY/80);
+        background.style.translate = x + 'px ' + y + 'px';
+    };
+};
